8857ff91c1130619720dafefef64678aa37a553a
Momin Raza <135518733+MominRaza6762@users.noreply.github.com>
2025-02-11 16:41:35 +0500

    Update index.js

--- Back-End/index.js
+++ Back-End/index.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const port =3000;
+const port =  process.env.PORT || 3000;;
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
